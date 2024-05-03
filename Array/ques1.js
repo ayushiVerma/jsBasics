556ea4274461476eef669a0e1faa222d0f31361b
ayushi <ayushi.verma@sony.com>
2024-05-03 10:26:44 +0530

    Leet code problem solvings

--- Array/ques1.js
+++ Array/ques1.js
@@ -1,29 +1,61 @@
-// 11. Container With Most Water
+// 35. Search Insert Position
+// Easy
+// Topics
+// Companies
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-
-// Find two lines that together with the x-axis form a container, such that the container contains the most water.
-
-// Return the maximum amount of water a container can store.
-
-// Notice that you may not slant the container.
+// You must write an algorithm with O(log n) runtime complexity.
 
  
 
 // Example 1:
 
-
-// Input: height = [1,8,6,2,5,4,8,3,7]
-// Output: 49
-// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
 // Example 2:
 
-// Input: height = [1,1]
+// Input: nums = [1,3,5,6], target = 2
 // Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
  
 
 // Constraints:
 
-// n == height.length
-// 2 <= n <= 105
-// 0 <= height[i] <= 104
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums contains distinct values sorted in ascending order.
+// -104 <= target <= 104
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let index = 0;
+    let p = 0;
+    for(var i = 0; i < nums.length; i++){
+        if(nums[i] === target){
+            return i;
+        }else{
+          
+           if(nums[i] < target){
+            p++;
+            if(p === nums.length){
+                return p;
+            }
+           }else{
+            return p;
+           }
+        }
+    }
+};
+
+
+
+console.log(searchInsert([1,3,5,6], 5))
+// console.log(searchInsert([1,3,5,6], 2))
+// console.log(searchInsert([1,3,5,6], 7))
